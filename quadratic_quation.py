4d376db6f89cb9317fdd98b401b5d0b6cef00555
Sanket Srivastav <135122553+Sanket-Kayastha@users.noreply.github.com>
2025-11-13 16:57:19 +0530

    Add files via upload

---
+++ quadratic_quation.py
@@ -0,0 +1,23 @@
+import math
+
+a =float(input("Enter the value of a: "))
+b = float(input("Enter the value of b: "))
+c =float(input("Enter the value of c: "))
+
+discriminant = b**2 - 4*a*c
+
+if discriminant> 0:
+    root1 = (-b + math.sqrt(discriminant))/(2*a)
+    root2 = (-b + math.sqrt(discriminant))/(2*a)
+    print(f"Root 1 is {root1}")
+    print(f"Root 2 is: {root2}")
+
+elif discriminant == 0:
+    root = -b/(2*a)
+    print(f"Root is: {root}")
+
+else:
+    real_part = -b/(2*a)
+    imaginary_part = math.sqrt(abs(discriminant))/(2*a)
+    print(f"Root 1: {real_part} + {imaginary_part}")
+    print(f"Root 2: {real_part} + {imaginary_part}")
